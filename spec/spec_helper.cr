bf4bac51e00d50d9bab68403993db9d328d7a0fc
Ryan Kraay <6840071+ryan-kraay@users.noreply.github.com>
2023-06-07 22:27:38 +0200

    Add Database unit test

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,6 @@
-require "../src/stremio-addon-server"
+require "../src/stremio-addon-sqlite3"
 require "spectator"
+
+# all the magic we need to have a database connection
+require "db"
+require "sqlite3"
--- spec/stremio-addon-server_spec.cr
+++ spec/stremio-addon-server_spec.cr
@@ -2,7 +2,25 @@ require "./spec_helper"
 
 Spectator.describe Stremio::Addon::Server do
 
+  let(addon) { Stremio::Addon::SQLite3.new DB.open("sqlite3:?journal_mode=wal&synchronous=normal") }
+
+  before_each {
+    # FYI:  let() and subject() are lazy-loaded, but are not accessible within a before_all {}
+    expect do
+      addon.create_table
+    end.to_not raise_error()
+  }
+
   it "works" do
     expect(false).to eq(false)
+  end
+
+  it "connects to a database" do
+    expect do
+      addon.conn.exec "insert into #{addon.table} values (?, ?)", "John Doe", 30
+    end.to_not raise_error()
+
+    # Check if we're recycling connections
+    expect(addon.conn.scalar "select max(age) from #{addon.table}").to eq(30)
   end
 end
